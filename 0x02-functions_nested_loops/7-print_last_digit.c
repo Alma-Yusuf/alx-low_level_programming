9e86cba9fb252e9312026c7fd837f3c4c9f97165
Alma-Yusuf <almaabeid2000@gmail.com>
2022-03-16 22:22:42 +0300

    made readable

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,8 +6,16 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = n * -1;
-	_putchar((n % 10) + '0');
-	return (n % 10);
+int lastDig;
+if (n >= 0)
+{
+lastDig = n % 10;
 }
+else
+{
+lastDig = (n % 10) * -1;
+}
+_putchar('0' + lastDig);
+return (lastDig);
+}
+
