9ac691f524236152ade55cf7616782c67c50654c
Alma-Yusuf <almaabeid2000@gmail.com>
2022-04-15 17:52:07 +0300

    new files

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,4 @@
-able File  10 lines (7 sloc)  255 Bytes
-   
-#ifndef _FUNCTION_POINTERS_
-#define _FUNCTION_POINTERS_
-
-#include <stdlib.h>
-
+int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
-#endif
