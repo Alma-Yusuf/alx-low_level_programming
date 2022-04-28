3e5b05ba45f0fb974c07879bdbff78c285675a28
Alma-Yusuf <almaabeid2000@gmail.com>
2022-04-28 12:29:36 +0300

    new files

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_binary - A function that prints a binary number without % or /
+ * @n: The number to be printed
+ * Return: Nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i, flag;
+
+	if (n == 0)
+	{
+		_putchar('0');
+	}
+	for (flag = 0, i = sizeof(n) * 8 - 1; i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+		{
+			flag = 1;
+		}
+		if (flag == 1)
+		{
+			((n >> i) & 1) ? _putchar('1') : _putchar('0');
+		}
+	}
+}
